7e4f34c6031e8e5fda7b2f2790aae313761e33f3
shawroger <roger@shawroger.com>
2019-10-01 21:29:42 +0800

    更新0.0.1

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,100 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div id="app">
+
+<br/><br/><br/><br/>
+<el-row type="flex" justify="center">
+<el-col :xs="24" :md="20" :lg="16" :xl="8">
+
+
+<center>
+<el-upload
+  class="upload-demo"
+  :on-success="uploadSuccess"
+  action="https://jsonplaceholder.typicode.com/posts/"
+  ref="file"
+  drag
+  >
+  <i class="el-icon-upload"></i>
+  <div class="el-upload__text">
+    将文件拖到此处，或<em>点击上传</em>
   </div>
+
+</el-upload>
+
+</center>
+
+<center>
+<div v-if="showTable">
+
+
+<el-divider></el-divider>
+<el-table
+    :data="table_source"
+    style="width: 100%"
+    align="center"
+    >
+    <el-table-column
+        type="index"
+    >
+    </el-table-column>
+    <el-table-column
+        v-for="(K,V,I) in tableHead"
+        :prop="V"
+        :label="K"
+        :key="I"
+        align="center"
+    >
+    </el-table-column>
+  
+  </el-table>
+
+</div>
+
+</center>
+</el-col>
+</el-row>    
+<br />
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
-  }
+  data:function(){
+        return{
+          showTable: false,
+          table_source:{},
+          tableWidth:0,
+          SOURCE:{},
+          tableHead:{},
+
+        }
+    },
+
+  methods: {
+
+    uploadSuccess(res,file) {
+
+      this.$rexine.rawFile(file.raw,(i,e)=>{
+          this.$rexine.read(e.target.result);
+          this.SOURCE = this.$rexine;
+          this.tableHead = this.SOURCE.json[0]
+          this.tableWidth = this.SOURCE.width;
+          this.table_source = this.SOURCE.json.slice(1);
+          this.showTable = true;
+      })
+
+    },
+
+
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body {
+    background-image: url('/bk.jpg')
+  }
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import ElementUI from 'element-ui';
+import Rexine from 'rexine';
+import Crearia from 'crearia';
+import 'element-ui/lib/theme-chalk/index.css';
 
-Vue.config.productionTip = false
+Rexine.use(Crearia);
+
+Rexine.TRIM = false
+
+Vue.config.productionTip = false;
+
+Vue.use(ElementUI);
+
+Vue.use(Rexine);
 
 new Vue({
+
   render: h => h(App),
+
 }).$mount('#app')
